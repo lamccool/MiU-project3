fc95536c34c431183586cc1b315e989f277d0f50
Laura McCool <mccool_laura@yahoo.com>
2012-02-15 21:02:21 -0600

    added files and did basic form elements

---
+++ Right/js/json.js
@@ -0,0 +1,273 @@
+var json = {
+	"gift1": {
+		"category":["Category:", "Electronics"], 
+		"comments":["Gift Description:", "Digital Camera"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Amazon"],
+		"url":["Product Url:","amazon.com"],
+		"date":["Date Added:","2011-12-01"]
+		},
+	"gift2": {
+		"category":["Category:", "Entertainment"], 
+		"comments":["Gift Description:", "Bluray"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Amazon"],
+		"url":["Product Url:","amazon.com"],
+		"date":["Date Added:","2011-11-27"]
+		},
+	"gift3": {
+		"category":["Category:", "Apparel"], 
+		"comments":["Gift Description:", "Jeans"],
+		"amount":["Quantity:","2"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Delias"],
+		"url":["Product Url:","delias.com"],
+		"date":["Date Added:","2011-11-30"]
+		},
+		"gift4": {
+		"category":["Category:", "Music"], 
+		"comments":["Gift Description:", "Keane CD"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Store"],
+		"store":["Store Name:","Keane Store"],
+		"url":["Product Url:","keanemusic.com"],
+		"date":["Date Added:","2011-10-01"]
+		},
+		"gift5": {
+		"category":["Category:", "Books"], 
+		"comments":["Gift Description:", "The Hunger Games Trilogy"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Amazon"],
+		"url":["Product Url:","amazon.com"],
+		"date":["Date Added:","2011-12-04"]
+		},
+		"gift6": {
+		"category":["Category:", "Apparel"], 
+		"comments":["Gift Description:", "Marc Jacobs Watch"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Nordstrom"],
+		"url":["Product Url:","http://shop.nordstrom.com"],
+		"date":["Date Added:","2011-11-25"]
+		},
+		"gift7": {
+		"category":["Category:", "Apparel"], 
+		"comments":["Gift Description:", "Socks"],
+		"amount":["Quantity:","4"],
+		"location":["Where To Buy:","Store"],
+		"store":["Store Name:","Target"],
+		"url":["Product Url:",""],
+		"date":["Date Added:","2011-12-12"]
+		},
+		"gift8": {
+		"category":["Category:", "Books"], 
+		"comments":["Gift Description:", "Let's Pretend This Never Happened: (A Mostly True Memoir) by Jenny Lawson"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Online"],
+		"url":["Product Url:","TheBloggess.com"],
+		"date":["Date Added:","2011-12-05"]
+		},
+		"gift9": {
+		"category":["Category:", "Entertainment"], 
+		"comments":["Gift Description:", "The Harry Potter movies on Bluray."],
+		"amount":["Quantity:","7"],
+		"location":["Where To Buy:","Store"],
+		"store":["Store Name:","Best Buy"],
+		"url":["Product Url:",""],
+		"date":["Date Added:","2011-11-30"]
+		},
+		"gift10": {
+		"category":["Category:", "Apparel"], 
+		"comments":["Gift Description:", "Jeans"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Delias"],
+		"url":["Product Url:","delias.com"],
+		"date":["Date Added:","2011-11-30"]
+		},
+		"gift11": {
+		"category":["Category:", "Etc"], 
+		"comments":["Gift Description:", "iTunes Gift Cards"],
+		"amount":["Quantity:","3"],
+		"location":["Where To Buy:","Store"],
+		"store":["Store Name:","Tom Thumb or Apple Store"],
+		"url":["Product Url:",""],
+		"date":["Date Added:","2011-11-26"]
+		},
+		"gift12": {
+		"category":["Category:", "Music"], 
+		"comments":["Gift Description:", "The Very Best of Nat King Cole CD"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Amazon"],
+		"url":["Product Url:","amazon.com"],
+		"date":["Date Added:","2011-12-04"]
+		},
+		"gift13": {
+		"category":["Category:", "Electronics"], 
+		"comments":["Gift Description:", "Keurig Single Cup Coffee Maker"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Keurig.com"],
+		"url":["Product Url:","www.keurig.com"],
+		"date":["Date Added:","2011-11-15"]
+		},
+		"gift14": {
+		"category":["Category:", "Etc"], 
+		"comments":["Gift Description:", "Green Tea single servings for Keurig"],
+		"amount":["Quantity:","25"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Keurig.com"],
+		"url":["Product Url:","www.keurig.com"],
+		"date":["Date Added:","2011-11-15"]
+		},
+		"gift15": {
+		"category":["Category:", "Etc"], 
+		"comments":["Gift Description:", "IKEA gift card"],
+		"amount":["Quantity:","2"],
+		"location":["Where To Buy:","Store"],
+		"store":["Store Name:","IKEA"],
+		"url":["Product Url:","ikea.com"],
+		"date":["Date Added:","2011-11-19"]
+		},
+		"gift16": {
+		"category":["Category:", "Etc"], 
+		"comments":["Gift Description:", "French Bulldog Puppy"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Store"],
+		"store":["Store Name:","Petco"],
+		"url":["Product Url:",""],
+		"date":["Date Added:","2011-12-14"]
+		},
+		"gift17": {
+		"category":["Category:", "Apparel"], 
+		"comments":["Gift Description:", "Toms Shoes - Gray"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Toms"],
+		"url":["Product Url:","www.toms.com"],
+		"date":["Date Added:","2011-11-03"]
+		},
+		"gift18": {
+		"category":["Category:", "Apparel"], 
+		"comments":["Gift Description:", "Sleeveless Maxi Dress- one black and one blue"],
+		"amount":["Quantity:","2"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Forever 21"],
+		"url":["Product Url:","forever21.com"],
+		"date":["Date Added:","2011-11-08"]
+		},
+		"gift19": {
+		"category":["Category:", "Entertainment"], 
+		"comments":["Gift Description:", "Justified Season 2"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Delias"],
+		"url":["Product Url:","delias.com"],
+		"date":["Date Added:","2011-11-30"]
+		},
+		"gift20": {
+		"category":["Category:", "Entertainment"], 
+		"comments":["Gift Description:", "Ferris Bueller's Day Off"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Amazon"],
+		"url":["Product Url:","amazon.com"],
+		"date":["Date Added:","2011-11-30"]
+		},
+		"gift21": {
+		"category":["Category:", "Electronics"], 
+		"comments":["Gift Description:", "iPad 2"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Store"],
+		"store":["Store Name:","Apple Store"],
+		"url":["Product Url:",""],
+		"date":["Date Added:","2011-11-27"]
+		},
+		"gift22": {
+		"category":["Category:", "Entertainment"], 
+		"comments":["Gift Description:", "Firefly Complete Series on Bluray"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Amazon"],
+		"url":["Product Url:","amazon.com"],
+		"date":["Date Added:","2011-11-10"]
+		},
+		"gift23": {
+		"category":["Category:", "Apparel"], 
+		"comments":["Gift Description:", "Wishbone charm bracelets"],
+		"amount":["Quantity:","2"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","etsy.com"],
+		"url":["Product Url:",""],
+		"date":["Date Added:","2011-11-12"]
+		},
+		"gift24": {
+		"category":["Category:", "Books"], 
+		"comments":["Gift Description:", "Born Confused by Tanuja Desai Hidier"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Store"],
+		"store":["Store Name:","Barnes and Noble"],
+		"url":["Product Url:",""],
+		"date":["Date Added:","2011-12-04"]
+		},
+		"gift25": {
+		"category":["Category:", "Music"], 
+		"comments":["Gift Description:", "Creedence Clearwater Revival: Greatest Hits"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Store"],
+		"store":["Store Name:","Good Records"],
+		"url":["Product Url:",""],
+		"date":["Date Added:","2011-12-13"]
+		},
+		"gift26": {
+		"category":["Category:", "Entertainment"], 
+		"comments":["Gift Description:", "Ferris Bueller's Day Off on DVD"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Amazon"],
+		"url":["Product Url:","amazon.com"],
+		"date":["Date Added:","2011-12-15"]
+		},
+		"gift27": {
+		"category":["Category:", "Entertainment"], 
+		"comments":["Gift Description:", "Princess Bride on Bluray"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Amazon"],
+		"url":["Product Url:","amazon.com"],
+		"date":["Date Added:","2011-12-16"]
+		},
+		"gift28": {
+		"category":["Category:", "Apparel"], 
+		"comments":["Gift Description:", "Betsy Johnson Blue Sunglasses"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Onlne"],
+		"store":["Store Name:","Betsy Johnson"],
+		"url":["Product Url:","betsyjohnson.com"],
+		"date":["Date Added:","2011-11-21"]
+		},
+		"gift29": {
+		"category":["Category:", "Electronics"], 
+		"comments":["Gift Description:", "Kitchen Aid Stand Mixer"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Store"],
+		"store":["Store Name:","Williams Sonoma"],
+		"url":["Product Url:",""],
+		"date":["Date Added:","2011-11-29"]
+		},
+		"gift30": {
+		"category":["Category:", "Etc"], 
+		"comments":["Gift Description:", "Target Gift Card"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Store"],
+		"store":["Store Name:","Target"],
+		"url":["Product Url:",""],
+		"date":["Date Added:","2011-11-24"]
+		},
+	}
+
---
+++ Wrong/js/main.js
@@ -0,0 +1,288 @@
+//Project 4
+//Visual Frameworks 1201
+//Mobile Media Development
+//Full Sail University
+//Laura McCool
+
+window.addEventListener ("DOMContentLoaded", function(){
+	function $(x){
+		var theElement = document.getElementById(x);
+		return theElement;
+	}
+	//create select field element and populate it with options.
+	function makeCats (){
+		var formTag = document.getElementsByTagName("form"), //formTag is an array of all form tags.
+			selectLi = $('select'),
+			makeSelect = document.createElement('select');
+			makeSelect.setAttribute("id", "category");
+		for (var i=0, j=giftCategory.length; i<j; i++){
+			 var makeOption = document.createElement('option');
+			 var optText = giftCategory[i];
+			 makeOption.setAttribute("value", optText);
+			 makeOption.innerHTML = optText;
+			 makeSelect.appendChild(makeOption);
+		}
+		//selectLi.appendChild(makeSelect);
+	}
+	
+	//Find value of selected radio button
+	function getSelectedRadio(){
+		var radios = document.forms(0).location;
+		for(var i=0; i<radios.length; i++){
+			if (radios[i].checked){
+				locationValue = radios[i].value;
+			}
+		}
+	}
+	
+	function toggleControls(n){
+		switch(n){
+			case "on":
+				$('giftForm').style.display ="none";
+				$('clear').style.display ="inline";
+				$('displayData').style.display ="none";
+				$('addNew').style.display = "inline";
+				break;
+			case "off":
+				$('giftForm').style.display ="block";
+				$('clear').style.display ="inline";
+				$('displayData').style.display ="inline";
+				$('addNew').style.display = "none";
+				$('items').style.display = "none";
+				break;
+			default:
+				return false;
+		}
+	}
+	
+    function($) {
+        $("#carousel1").carousel();
+        $("#carousel2").carousel({direction: "vertical"});
+    })(jQuery);
+	
+	
+	
+	function storeData(key){
+	// If there is no key this means this is a brand new item and we need a new key
+		if (!key){
+			var id 				= Math.floor(Math.random()*100000001);
+		}else{
+			//set the id to the exisiting key we're editing so that it will save over the data.
+			//the key is teh same key that's been passed along from the editSubmit event handler
+			//to validate function and then passed here into the storeData function
+			id = key;
+		}
+		//Gather up all our form field values and store in an object
+		//Object properties contain array with the form label and input value.
+		getSelectedRadio();
+		var item 				={};
+			item.category 	 	= ["Gift Category:", $('category').value];
+			item.comments 	 	= ["Gift Description:", $('comments').value];
+			item.amount 	 	= ["Quantity:", $('amount').value];
+			item.location 	 	= ["Where To Buy:", locationValue];
+			item.store 	 		= ["Store Name:", $('store').value];
+			item.url	 	 	= ["Product Url:", $('url').value];
+			item.date	 	 	= ["Date Added:", $('date').value];
+		
+		//Save Data in localStorage: Use Stringify to convert our object to a string.
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Item Added!");
+	}
+
+	function getData(){
+		toggleControls("on");
+		//write data from local storage to browser.
+		if(localStorage.length === 0){
+			alert("There is no data in Local Storage so default data was added.");
+			autoFillData();
+		}
+		var makeDiv = document.createElement('div');
+		makeDiv.setAttribute("id", "items");
+		var makeList = document.createElement('ul');
+		makeDiv.appendChild(makeList);
+		document.body.appendChild(makeDiv);
+		$('items').style.display = "block";
+		for(var i=0, len=localStorage.length; i<len;i++){
+			var makeli = document.createElement('li');
+			var linksLi = document.createElement ('li');
+			makeList.appendChild(makeli);
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			//Convert string from local storage value back to an object by using JSON.parse
+			var obj = JSON.parse(value);
+			var makeSubList = document.createElement('ul');
+			makeli.appendChild(makeSubList);
+			getImage(obj.category[1], makeSubList);
+			for(var n in obj){
+				var makeSubli = document.createElement('li');
+				makeSubList.appendChild(makeSubli);
+				var optSubText = obj[n][0]+" "+obj[n][1];
+				makeSubli.innerHTML = optSubText;
+				makeSubli.appendChild (linksLi);
+			}
+			makeItemLinks(localStorage.key(i), linksLi); //Edit and delete buttons for Local Storage
+		}
+	}
+	//Get image for the right category
+	function getImage(catName, makeSubList){
+		var imageLi = document.createElement('li');
+		makeSubList.appendChild(imageLi);
+		var newImg = document.createElement('img');
+		var setSrc = newImg.setAttribute("src","images/"+ catName +".png");
+		imageLi.appendChild(newImg);
+	}
+	//Make Item Links Function
+	//Create edit and delete links for each stored item when displayed
+	
+	function makeItemLinks (key, linksLi){
+	//add edit single item link
+		var editLink = document.createElement ('a');
+		editLink.href = "#";
+		editLink.key = key;
+		var editText ="Edit List";
+		editLink.addEventListener("click", editItem);
+		editLink.innerHTML = editText;
+		linksLi.appendChild (editLink);
+		
+		//add line break
+		var breakTag = document.createElement('br');
+		linksLi.appendChild(breakTag);
+		
+		//delete single item link
+		var deleteLink = document.createElement('a');
+		deleteLink.href = "#";
+		deleteLink.key = key;
+		var deleteText = "Delete Item"
+		deleteLink.addEventListener("click", deleteItem);
+		deleteLink.innerHTML = deleteText;
+		linksLi.appendChild (deleteLink);
+	}
+	//Auto Populate Local Storage
+	function autoFillData(){
+		for(var n in json){
+			var id = Math.floor(Math.random()*100000001);
+			localStorage.setItem(id,JSON.stringify(json[n]));
+		}
+	
+	}
+	
+
+	
+	function editItem (){
+		//grab date from local storage
+		var value = localStorage.getItem(this.key);
+		var item = JSON.parse(value);
+		
+		//show form
+		toggleControls("off");
+		
+		//populate form fields with current local storage values
+		$('category').value = item.category[1];
+		$('comments').value = item.comments[1];
+		$('amount').value = item.amount[1];
+		var radios = document.forms[0].location;
+		for(var i=0; i<radios.length; i++){
+			if(radios.value == "online" && item.location[1] == "online"){
+					radios[i].setAttribute("checked", "checked");
+				} else if (radios[i].value == "store" && item.location[1] == "store"){
+					radios[i].setAttribute("checked", "checked");
+				}
+		}
+		//$('location').value = item.location[1];
+		$('store').value = item.store[1];
+		$('url').value = item.url[1];
+		$('date').value = item.date[1];
+		
+		//remove the initial listener from input 'save contact' button
+		save.removeEventListener ("click", storeData);
+		//Change submit button value to say edit button
+		$('submit').value = "Edit List";
+		var editSubmit = $('submit');
+		//save the key value established in this function as a property of the editSubmit event
+		//so we can use that value when we save the date we edited 
+		editSubmit.addEventListener("click", validate);
+		editSubmit.key = this.key;		
+	}
+	
+	function deleteItem(){
+		var ask = confirm("Are you sure you want to delete this item?")
+		if (ask){
+			localStorage.removeItem(this.key);
+			alert("Item was deleted.");
+			window.location.reload();
+		}else{
+			alert("Item was NOT deleted.")
+		}
+	}
+	
+	function clearLocal(){
+		if(localStorage.length === 0){
+			alert("There is no data to clear.")
+		}else{ 
+			localStorage.clear();
+			alert("All items are deleted.");
+			window.location.reload();
+			return false;	
+		}
+	}
+	
+	function validate(e){
+		//define elements we want to check
+		var getCategory = $('category');
+		var getComments = $('comments');
+		var getAmount = $('amount');
+		
+		//Reset Error Message
+		errMsg.innerHTML ="";
+		getCategory.style.border = "1px solid black";
+		getComments.style.border = "1px solid black";
+		getAmount.style.border = "1px solid black";
+		
+		//Get Error Messages
+		var messageAry = [];
+		//Category Validation
+		if (getCategory.value === "--Choose A Gift Category--"){
+			var categoryError = "Please choose a category.";
+			getCategory.style.border = "1px solid red";
+			messageAry.push(categoryError);
+		}
+		
+		//Comments validation
+		if (getComments.value === ""){
+			var commentsError = "Please include your item.";
+			getComments.style.border = "1px solid red";
+			messageAry.push(commentsError);
+		}
+		
+		//If there were errors display them on the screen
+		if(messageAry.length >= 1){
+			for(var i=0, j=messageAry.length; i < j; i++){
+				var txt = document.createElement('li');
+				txt.innerHTML = messageAry[i];
+				errMsg.appendChild(txt);
+			}	
+			e.preventDefault();
+			return false;	
+		}else {
+			//If all is well save our data. send the key value (which came from editData function
+			//remember this key value was passed thru the editSubmit event listener as a property
+			storeData(this.key);
+		}
+
+	}
+	
+	//var defaults
+	var giftCategory = ["--Choose A Gift Category--", "Electronics", "Entertainment", "Apparel", "Music", "Books", "Etc"],
+		locationValue
+		errMsg = $('errors');
+	;
+	makeCats();
+	//set link and click events
+	var displayData = $('displayData');
+	displayData.addEventListener("click", getData);
+	var clearData =$('clear');
+	clearData.addEventListener("click", clearLocal);
+	var save = $('submit');
+	save.addEventListener("click", validate);
+
+});
